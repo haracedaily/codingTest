5bc5040234629cb432ecb772ad676a3970f813d5
준호 <123717055+haracedaily@users.noreply.github.com>
2025-02-19 14:35:25 +0900

    [level 0] Title: 이진수 더하기, Time: 3.66 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,12 @@
 function solution(bin1, bin2) {
-    return (parseInt(bin1,2)+parseInt(bin2,2)).toString(2);
+    return (change_binary(bin1.split("").reverse())+change_binary(bin2.split("").reverse())).toString(2);
+}
+
+function change_binary(a){
+    let res =0;
+    for(let i = 0; i<a.length; i++){
+        if(a[i]==1)res+=2**i;
+    }
+    console.log(res);
+    return res;
 }
