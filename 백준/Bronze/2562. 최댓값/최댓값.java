565924c9e1c30768851a71dd3f74119ac18060c1
준호 <123717055+haracedaily@users.noreply.github.com>
2025-08-10 20:00:38 +0900

    [Bronze III] Title: 최댓값, Time: 188 ms, Memory: 18380 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int index = 0;
+        for(int i=1;i<=9;i++){
+            int num = sc.nextInt();
+            if(num>max){
+                max=num;
+                index=i;
+            }
+        }
+        sc.close();
+        System.out.println(max+"\n"+index);
+    }
+}
