4742aed526d3da843dfd907c1b5bc0b7ff89e4cd
준호 <123717055+haracedaily@users.noreply.github.com>
2025-08-10 19:48:41 +0900

    [Bronze III] Title: 최소, 최대, Time: 1536 ms, Memory: 310940 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,21 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < cnt; i++) {
+        int min = 1000001;
+        int max = -1000001;
+        for(int i = 1; i<=cnt; i++){
             int num = sc.nextInt();
-            if (num < min) {
+            if(num<min){
                 min = num;
             }
-            if (num > max) { // else if 아님
+            if(num>max){
                 max = num;
             }
         }
-
         sc.close();
-        System.out.println(min + " " + max);
+        System.out.println(min+" "+max);
     }
 }
