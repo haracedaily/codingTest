2f070c14039b23cacf2217db555d4ff3af096e05
준호 <123717055+haracedaily@users.noreply.github.com>
2025-08-24 19:39:30 +0900

    [level 1] Title: 문자열 나누기, Time: 0.92 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,26 @@
+function solution(s) {
+    let answer = 0;
+    let same = 0, diff = 0;
+    let x = s[0];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === x) {
+            same++;
+        } else {
+            diff++;
+        }
+
+        if (same === diff) {
+            answer++;
+            same = 0;
+            diff = 0;
+            x = s[i + 1];
+        }
+    }
+
+    if (same !== 0 || diff !== 0) {
+        answer++;
+    }
+
+    return answer;
+}
