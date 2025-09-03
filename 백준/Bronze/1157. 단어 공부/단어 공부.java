7e611293a4a6dcdef627a3f0ef3f24870f358730
준호 <123717055+haracedaily@users.noreply.github.com>
2025-09-03 20:44:16 +0900

    [Bronze I] Title: 단어 공부, Time: 408 ms, Memory: 28988 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+        int[] arr = new int[26];
+        for (int i = 0; i < str.length(); i++){
+		    arr[str.charAt(i) - 65]++;
+	    }
+        int max = -1;
+        char ch = '?';
+ 
+        for (int i = 0; i < 26; i++) {
+	        if (arr[i] > max) {
+	        	max = arr[i];
+	        	ch = (char) (i + 65);
+        	}
+        	else if (arr[i] == max) {
+	    	ch = '?';
+        	}
+        }
+        System.out.print(ch);
+    }
+}
